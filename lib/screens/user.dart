49ef0accc24caa29a0d56bf7d30fd82bd970df40
Shubha Rajan <shubharajan94@gmail.com>
2019-07-17 14:30:33 -0700

    can accept friend requests. user page is stateful now

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -6,26 +7,92 @@ import 'package:playlist_for_two/helpers/login_helper.dart';
 
 
 
-class UserPage extends StatelessWidget {
+class UserPage extends StatefulWidget {
   UserPage({Key key, this.name, this.userID}) : super(key: key);
   final String name;
   final String userID;
 
   @override
-  Widget build(BuildContext context) {
+  _UserPageState createState() => _UserPageState();
+}
 
-    void _requestFriend() async {
+class _UserPageState extends State<UserPage> {
+
+  String _friendStatus;
+
+
+  Future<Map> getFriends() async {
+    String token = await LoginHelper.getAuthToken();
+    String userID = await LoginHelper.getLoggedInUser();
+
+    var response = await http.get("${DotEnv().env['P42_API']}/friends?user_id=$userID", headers: {'authorization': token});
+
+    return json.decode(response.body);
+  }
+
+
+  void setFriendStatus(id) async{
+    var data = await getFriends();
+    var _friends= data['friends'];
+    print(_friends);
+    dynamic incoming = _friends['incoming'].map((friend) => json.decode(friend)['friend_id']);
+    dynamic requested = _friends['sent'].map((friend) => json.decode(friend)['friend_id']);
+    dynamic accepted = _friends['accepted'].map((friend) => json.decode(friend)['friend_id']);
+
+    if (incoming.contains(id)) {
+      setState(() {
+      _friendStatus= 'incoming';
+    });
+    } else if (requested.contains(id)) {
+      setState(() {
+      _friendStatus= 'requested';
+    });
+    } else if (accepted.contains(id)) {
+      setState(() {
+      _friendStatus= 'accepted';
+    });
+    } else {
+      setState(() {
+      _friendStatus= 'none';
+    });
+    }
+  }
+
+  initState() {
+    super.initState();
+    setFriendStatus(widget.userID);
+  }
+
+  void _requestFriend() async {
         String currentUser = await LoginHelper.getLoggedInUser();
         String token = await LoginHelper.getAuthToken();
         var payload = {
           "user_id": currentUser,
-          "friend_id": userID,
+          "friend_id": widget.userID,
         };
         dynamic response = await http.post("${DotEnv().env['P42_API']}/request-friend", headers:{"authorization":token}, body:payload);
 
         print(response.body);
-    }
+  }
 
+  void _acceptFriend() async {
+    String currentUser = await LoginHelper.getLoggedInUser();
+    String token = await LoginHelper.getAuthToken();
+    var payload = {
+          "user_id": currentUser,
+          "friend_id": widget.userID,
+        };
+    dynamic response = await http.post("${DotEnv().env['P42_API']}/accept-friend", headers:{"authorization":token}, body:payload);
+
+    print(response.body);
+  }
+
+  void _viewPlaylist() async {
+    print('TODO: Write me!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Playlist for Two"),
@@ -34,11 +101,23 @@ class UserPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(name,
+            Text(widget.name,
             style: TextStyle(fontSize: 50), textAlign: TextAlign.center,),
             SizedBox(height: 30),
-            MaterialButton(
-              onPressed: _requestFriend,
+            _actionButton(context, _friendStatus, _requestFriend, _acceptFriend, _viewPlaylist)
+          ]
+        )
+      )
+    );
+  }
+}
+Widget _actionButton(BuildContext context, String status, Function requestFriend, Function acceptFriend, Function viewPlaylist) {
+
+  Widget button;
+  switch(status) {
+    case 'none': {
+      button = MaterialButton(
+              onPressed: requestFriend,
               child: Text('Send a Friend Request',
                     style: TextStyle(fontSize: 20)
                   ),
@@ -47,10 +126,51 @@ class UserPage extends StatelessWidget {
                   color:Colors.blueAccent, 
                   height: 50,
                   minWidth:300
-            )
-          ]
-        )
-      )
-    );
+            );
+    }
+    break;
+    case 'requested': {
+      button = MaterialButton(
+              onPressed: null,
+              child: Text('Friend Request Sent',
+                    style: TextStyle(fontSize: 20)
+                  ),
+                  shape: StadiumBorder(),
+                  textColor: Colors.white,
+                  color:Colors.blueAccent, 
+                  height: 50,
+                  minWidth:300
+            );
+    }
+    break;
+    case 'accepted': {
+      button = MaterialButton(
+              onPressed: viewPlaylist,
+              child: Text('Show Playlist',
+                    style: TextStyle(fontSize: 20)
+                  ),
+                  shape: StadiumBorder(),
+                  textColor: Colors.white,
+                  color:Colors.blueAccent, 
+                  height: 50,
+                  minWidth:300
+            );
+    }
+    break;
+    case 'incoming': {
+      button = MaterialButton(
+              onPressed: acceptFriend,
+              child: Text('Accept Friend Request',
+                    style: TextStyle(fontSize: 20)
+                  ),
+                  shape: StadiumBorder(),
+                  textColor: Colors.white,
+                  color:Colors.blueAccent, 
+                  height: 50,
+                  minWidth:300
+            );
+    }
+    break;
   }
+  return(button);
 }
